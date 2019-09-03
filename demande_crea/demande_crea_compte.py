b9f3c38a001062369f74b58ab219a20460f01133
Moi <iu>
2019-09-03 15:28:50 +0200

    comi

--- demande_crea/demande_crea_compte.py
+++ demande_crea/demande_crea_compte.py
@@ -1,7 +1,7 @@
 """Le front renvoi un dictionnaire contenant toutes les valeurs"""
 
 
-class DemandCreaCompte:
+class DemandCreaCompte :
     def __init__(self,valeur:dict):
         self.nom
         self.prenom
@@ -15,7 +15,25 @@ class DemandCreaCompte:
 
 
     def enregistrement(self): #Stocakge d'une demmande dans la base de donnee (table demande)
-        return bool
+
+
+        cnx = connection.MySQLConnection(user='root', password='', host='127.0.0.1', database='Gestbank')
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+
+
+
+        try:
+            insert_stmt = ("INSERT into demande_creation (nom,prenom,id,mail,telephone,adresse,justificatif)" "VALUES (%s ,%s, %s, %s,%s ,%s, %s)")
+            data = [(self.nom,self.prenom,self.id,self.mail,self.tel,self.adresse,self.justificatif)]
+            cursor.execute(insert_stmt, data)
+            cnx.commit()
+        except :
+            return False
+
+        else :
+            return  True
+
 
 
     def affectation(self,agent): #l'admin affect un client a un agent
