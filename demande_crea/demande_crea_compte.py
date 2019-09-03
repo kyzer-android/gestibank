3d76a27938b7acb0f87ae6995b5058b2f117b340
GK <test@g.com>
2019-09-03 13:52:13 +0200

    first commit

---
+++ demande_crea/demande_crea_compte.py
@@ -0,0 +1,33 @@
+"""Le front renvoi un dictionnaire contenant toutes les valeurs"""
+
+
+class DemandCreaCompte:
+    def __init__(self,valeur:dict):
+        #self.(donne crea compte)
+        self.valid
+        self.affect
+        pass
+
+    def enregistrement(self): #Stocakge d'une demmande dans la base de donnee (table demande)
+        return bool
+
+
+    def affectation(self,agent): #l'admin affect un client a un agent
+        return bool
+
+    def validation(self, valide) :  #L'agent valide le client
+        return bool
+
+
+class User:
+    self.id
+
+
+""""
+  def lister_demande(self): #Affiche toutes les demande en cours
+        return list(dict)
+
+    def selectionner_une_demande(self, dict, pos):  # recoit la list(dict) de lister_demande et renvoi un dict
+        return dict[pos]
+
+"""
