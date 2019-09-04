e5a82810ccea07bdfa03c9f22931958959ecbab8
GK <test@g.com>
2019-09-04 13:44:12 +0200

    mise a jour

--- demande_crea/user.py
+++ demande_crea/user.py
@@ -2,21 +2,50 @@ import mysql.connector as mysql
 import logging
 from sql import connexion
 logging.basicConfig(level=logging.DEBUG)
+class User:
+    def __init__(self,type_user,id):
+        cnx = connexion()
+        cursor = cnx.cursor()
+        print(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = '" + type_user + "' AND table_schema='Gestibank';")
+        cursor.execute(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = '" + type_user + "' AND table_schema='Gestibank';")
+        res=cursor.fetchall()
 
-def affiche_donne_curs(curs):
-    for ligne in curs:
-        print(ligne)
+        for colone in res:
+            command=("Select " + colone[0] + " from " + type_user + " where id = " + id)
+            print(command)
+            cursor.execute(command)
+            value=cursor.fetchone()
+            self.__setattr__(colone[0],value[0] )
 
 
-def identification(ID):
-        Statut=False
-        cnx=connexion()
-        cursor=cnx.cursor()
-        cursor.execute("SELECT * FROM `login` WHERE ID = "+ID)
-        row=cursor.fetchone()
-        if cursor.rowcount==-1
-            logging.info("")
+    def affiche_donne_curs(curs):
 
-identification("0020")
+        for ligne in curs:
+            print(ligne)
+
+
+    def identification(ID):
+            Statut=False
+            cnx=connexion()
+            cursor=cnx.cursor()
+            cursor.execute("SELECT * FROM `login` WHERE ID = "+ID)
+            row=cursor.fetchone()
+            if cursor.rowcount==-1:
+                print("Utilisateur innconnu")
+            elif cursor.rowcount==1:
+                 print(row)
+            else :
+                print("erreur de paramètre")
+
+def affiche_client(client):
+    print("nom:{},Prenom :{},")
+    type_user="client"
+    id="0020"
+
+u=User("client","0020")
+print(u)
+
 
 
