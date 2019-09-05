9d4d9522d44f836a9ef1871b614ab5cbc8de3918
GK <test@g.com>
2019-09-05 12:08:03 +0200

    mise a jour demand crea compte

--- compte_user/admin.py
+++ compte_user/admin.py
@@ -1,12 +1,13 @@
 import logging
 from sql.sql import connexion
 from compte_user.user import User
+from demande_crea.demande_crea_compte import DemandCreaCompte
 
 class Admin(User):
     # cnx = connexion()
 
     def __init__(self, id):
-        super().__init__("ADMIN", id)
+        super().__init__("ADMIN",id)
 
 
     def __str__(self):
@@ -26,3 +27,32 @@ class Admin(User):
                          "email": self.email,
                          "tel": self.tel})
         User.update("agent", self.id, **list_arg)
+
+    def lister_demand_crea(self):
+        cnx=connexion()
+        cursor=cnx.cursor()
+        cursor.execute("SELECT * FROM `demande_creacompte`")
+        liste_obj=[]
+        for element in cursor:
+            liste_obj.append(DemandCreaCompte(element))
+
+        return liste_obj
+
+    def __str__(self):
+        test = (self.id,
+                self.nom,
+                self.prenom,
+                self.type_user,
+                self.email,
+                self.tel
+                )
+
+        return str(test)
+if __name__ == "__main__":
+    Objtest=Admin("0014")
+    print(Objtest)
+    liste_demande = Objtest.lister_demand_crea()
+    i=0
+    for liste in liste_demande:
+        i+=1
+        print(liste_demande[i])
